fc68ed414433de03df53c77570923a5cfdb0c4f4
Calvin-Davidson <6a3x6bn4>
2020-02-28 11:35:20 +0100

    adding the first project

---
+++ Berend-Les/Assets/scripts/Player/PlayerClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private PlayerManager playerManager;
+    void Start()
+    {
+        this.playerManager = GetComponent<PlayerManager>();
+    }
+
+  
+    private void OnMouseDown()
+    {
+        this.playerManager.playerdata.setHealth(playerManager.playerdata.getHealth() - 10);
+    }
+}
---
+++ Berend-Les/Assets/scripts/Player/PlayerData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData {
+    private Text healtText;
+
+    public PlayerData()
+    {
+        healtText = GameObject.Find("HealtText").GetComponent<Text>();
+    }
+
+    private int _health = 100;
+
+
+    public int getHealth()
+    {
+        return this._health;
+    }
+
+    public void setHealth(int value)
+    {
+        this._health = value;
+        healtText.text = "Health: " + _health;
+
+        if (_health <= 0)
+        {
+            Debug.Log("ik ben dood :(");
+        }
+    }
+}
